2189a31bb331ad860b83018dcea0410572fe7f62
Simon Müller <s.mueller.ka@hotmail.com>
2017-06-22 19:49:00 +0200

    First Commit

---
+++ ResourcesLua/CredentialInputForm.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResourcesLua
+{
+    /// <summary>
+    /// Interaktionslogik für CredentialInputForm.xaml
+    /// </summary>
+    public partial class CredentialInputForm : Window
+    {
+
+        string apiEnterMessage = "Enter API key here";
+        string workshopIdEnterMessage = "Enter collection ID here";
+
+        public CredentialInputForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(((TextBox)sender).Text == "")
+            {
+                ((TextBox)sender).Text = apiEnterMessage;
+            }
+        }
+
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (((TextBox)sender).Text == apiEnterMessage)
+            {
+                ((TextBox)sender).Text = "";
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow(textBox.Text, textBox_Copy.Text);
+            m.Show();
+            this.Close();
+        }
+
+        private void textBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (((TextBox)sender).Text == "")
+            {
+                ((TextBox)sender).Text = workshopIdEnterMessage;
+            }
+        }
+
+        private void textBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (((TextBox)sender).Text == workshopIdEnterMessage)
+            {
+                ((TextBox)sender).Text = "";
+            }
+        }
+    }
+}
---
+++ ResourcesLua/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ResourcesLua
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string steamApiKey;
+        private string collectionID = "249554398";
+        private string steamApiUrl = "https://api.steampowered.com";
+        HttpClient client;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(string steamApiKey, string collectionID)
+        {
+            this.collectionID = collectionID;
+
+            InitializeComponent();
+            this.steamApiKey = steamApiKey;
+            client = new HttpClient();
+            var postParameters = new Dictionary<string, string>
+            {
+                { "format", "json" },
+                {"key", steamApiKey },
+                {"publishedfileids[0]",  collectionID},
+                {"collectioncount", "1" }
+            };
+            var content = new FormUrlEncodedContent(postParameters);
+            var response = client.PostAsync(steamApiUrl + "/ISteamRemoteStorage/GetCollectionDetails/v0001/", content).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Api Key might be invalid!");
+            }
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            MessageBox.Show(responseString);
+        }
+    }
+}
